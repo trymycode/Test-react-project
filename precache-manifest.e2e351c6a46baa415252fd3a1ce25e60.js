4d4b3a7dc28f212562d22299032f16710adc0633
trymycode <imitasingha1994@gmail.com>
2020-04-05 14:14:45 +0530

    Updates

---
+++ precache-manifest.e2e351c6a46baa415252fd3a1ce25e60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "986eec92743de1f4f11f24de99e300c2",
+    "url": "/Test-react-project/index.html"
+  },
+  {
+    "revision": "a91d013e66c04f360c8c",
+    "url": "/Test-react-project/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "79756cebc3289cfa7621",
+    "url": "/Test-react-project/static/css/main.bf3ac47b.chunk.css"
+  },
+  {
+    "revision": "a91d013e66c04f360c8c",
+    "url": "/Test-react-project/static/js/2.75ee1d7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Test-react-project/static/js/2.75ee1d7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79756cebc3289cfa7621",
+    "url": "/Test-react-project/static/js/main.231c77a5.chunk.js"
+  },
+  {
+    "revision": "bd2b945dd5e3aa8e5598",
+    "url": "/Test-react-project/static/js/runtime-main.92216491.js"
+  }
+]);
